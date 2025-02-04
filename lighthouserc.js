1a4dad93af4217692d44cc280a51c395f2f94c70
Daniel Efe <daneytechwriter@gmail.com>
2025-02-04 10:23:20 -0800

    new commit

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,17 @@
-export const ci = {
-    ci: {
-      upload: {
-        target: 'temporary-public-storage',
-      },
-    },
-  };
+export const ci = { 
+    collect: { url: ['http://localhost:3000'], 
+    }, 
+    assert: { 
+        assertions: { 
+            'categories:seo': 'off', 
+            'categories:best-practices': 'off', 
+            'categories:pwa': 'off', 
+            
+             'categories:performance': ['error', { minScore: 0.7 }], 
+             'categories:accessibility': ['error', { minScore: 0.8 }], 
+              },
+             }, 
+             upload: { 
+                target: 'temporary-public-storage', 
+            }, 
+        };
